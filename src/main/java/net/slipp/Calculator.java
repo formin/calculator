4bf6c07091d49f61358174c3a3d96093d25db3db
formin <formin@programer.pe.kr>
2013-07-23 09:26:54 +0900

    first commit

--- src/main/java/net/slipp/Calculator.java
+++ src/main/java/net/slipp/Calculator.java
@@ -1,20 +1,22 @@
 package net.slipp;
 
+import java.util.regex.Pattern;
+
 public class Calculator {
 
 	public int add(int i, int j) {
-		// TODO Auto-generated method stub
+		 
 		return i+j;
 	}
 
 	public int subtract(int i, int j) {
-		// TODO Auto-generated method stub
+		 
 		return i-j;
 	}
   
 
 	public int calculator(String s) {
-		// TODO Auto-generated method stub
+		 
 		if (s.isEmpty()) { 
 			return 0;
 		}
@@ -29,17 +31,37 @@ public class Calculator {
 
 	public String[] split(String s)
 	{
-		String[] numbers = s.split(",|\n|//");
+		String[] numbers;
+		
+		java.util.regex.Matcher m = Pattern.compile("//(.)\n(.*)").matcher(s);
+		m.find();
+
+		numbers = s.split(",|\n");
+		
+		if(m.matches()){
+		 String customDelimeter = m.group(1);
+		 numbers = m.group(2).split(customDelimeter);
+		}
+		 
 		return numbers;
 	}
 	
 	public int cal(String[] numbers){
 
 		int s = 0;
+		int chk = 0;
 		
 		for(int i=0;i<numbers.length;i++)
 		{
-			s += Integer.parseInt(numbers[i]);
+			
+			chk = Integer.parseInt(numbers[i]);
+			
+			if(chk<0)
+			{
+				throw new RuntimeException();
+			}
+			s += chk;
+			
 		}
 		
 		return s;
--- src/test/java/net/slipp/CalculatorTest.java
+++ src/test/java/net/slipp/CalculatorTest.java
@@ -33,4 +33,47 @@ public class CalculatorTest {
 		int result = cal.calculator(s);  
 		assertEquals(0,result); 
 	}
+
+	@Test
+	public void calculator2(){
+		String s = "1";
+		int result = cal.calculator(s);  
+		assertEquals(1,result); 
+	}
+
+	@Test
+	public void calculator3(){
+		String s = "1,2";
+		int result = cal.calculator(s);  
+		assertEquals(3,result); 
+	}
+
+	@Test
+	public void calculator4(){
+		String s = "1,2,3";
+		int result = cal.calculator(s);  
+		assertEquals(6,result); 
+	}
+
+	@Test
+	public void calculator5(){
+		String s = "1,2\n3";
+		int result = cal.calculator(s);  
+		assertEquals(6,result); 
+	}
+
+	@Test
+	public void calculator6(){
+		String s = "//;\n1;2;3";
+		int result = cal.calculator(s);  
+		assertEquals(6,result); 
+	}
+
+	@Test
+	public void calculator7(){
+		String s = "-1,2,3";
+		int result = cal.calculator(s);  
+		assertEquals(4,result); 
+	}
+	
 }
